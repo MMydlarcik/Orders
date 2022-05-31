41744908d4bf0a834878e92fccf803a9eac3cd66
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-05-31 18:50:30 +0200

    Update users.blade.php

--- resources/views/home/users.blade.php
+++ resources/views/home/users.blade.php
@@ -1,16 +1,35 @@
 @extends('layouts.app-master')
 
 @section('content')
-    <div class="bg-light p-5 rounded">
-        @auth
-        <h1>Dashboard</h1>
-        <p class="lead">Only authenticated users can access this section.</p>
-        <a class="btn btn-lg btn-primary" href="https://codeanddeploy.com" role="button">View users here &raquo;</a>
-        @endauth
-
-        @guest
-        <h1>Homepage</h1>
-        <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
-        @endguest
+<div class="bg-light p-5 rounded">
+    @auth
+    <h1>Users list</h1>
+    <p class="lead">Only authenticated users can access this section.</p>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($users as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->username }}</td>
+                    <td>{{ $item->email }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+    @endauth
+
+    @guest
+    <h1>Homepage</h1>
+    <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
+    @endguest
+</div>
 @endsection
