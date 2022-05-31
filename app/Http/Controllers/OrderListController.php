e939499179d83b5b6751df014a907aeec360d567
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-05-31 18:50:11 +0200

    Update OrderListController.php

--- app/Http/Controllers/OrderListController.php
+++ app/Http/Controllers/OrderListController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
+
 
 class OrderListController extends Controller
 {
     public function orderList() 
     {
-        return view('home.orders');
+        $orders = Order::all();
+        return view('home.orders')->with([
+            'orders'=> $orders
+        ]);
     }
 }
