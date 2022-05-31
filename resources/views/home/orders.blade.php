9a5ba109a2dd23401f22ea152c3093f01c1ef711
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-05-31 18:50:23 +0200

    Update orders.blade.php

--- resources/views/home/orders.blade.php
+++ resources/views/home/orders.blade.php
@@ -1,16 +1,35 @@
 @extends('layouts.app-master')
 
 @section('content')
-    <div class="bg-light p-5 rounded">
-        @auth
-        <h1>Dashboard</h1>
-        <p class="lead">Only authenticated users can access this section.</p>
-        <a class="btn btn-lg btn-primary" href="https://codeanddeploy.com" role="button">View orders here &raquo;</a>
-        @endauth
-
-        @guest
-        <h1>Homepage</h1>
-        <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
-        @endguest
+<div class="bg-light p-5 rounded">
+    @auth
+    <h1>Order list</h1>
+    <p class="lead">Only authenticated users can access this section.</p>
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($orders as $item)
+                <tr>
+                    <td>{{ $item->id }}</td>
+                    <td>{{ $item->code }}</td>
+                    <td>{{ $item->author_id }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
+    @endauth
+
+    @guest
+    <h1>Homepage</h1>
+    <p class="lead">Your viewing the home page. Please login to view the restricted data.</p>
+    @endguest
+</div>
 @endsection
