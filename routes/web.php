6198988dd2a76db832490a942a9834cb102e89d0
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-02 19:29:48 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'App\Http\Controllers'], function()
-{   
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
     /**
      * Home Routes
      */
     Route::get('/', 'HomeController@index')->name('home.index');
 
-    Route::group(['middleware' => ['guest']], function() {
+    Route::group(['middleware' => ['guest']], function () {
         /**
          * Register Routes
          */
@@ -32,15 +31,27 @@ Route::group(['namespace' => 'App\Http\Controllers'], function()
          */
         Route::get('/login', 'LoginController@show')->name('login.show');
         Route::post('/login', 'LoginController@login')->name('login.perform');
-
+        
     });
 
-    Route::group(['middleware' => ['auth']], function() {
+    Route::group(['middleware' => ['auth']], function () {
         /**
          * Logout Routes
          */
         Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
-        Route::get('/orders', 'OrderListController@orderList')->name('home.orders');
-        Route::get('/users', 'UserListController@userList')->name('home.users');
+        /**
+         * Order Routes
+         */
+        Route::post('/orders/create', 'OrderListController@store')->name('orders.store');
+        Route::get('/orders/create', 'OrderListController@create')->name('orders.create');
+        Route::post('/orders/destroy', 'OrderListController@destroy')->name('orders.destroy');
+        Route::get('/orders/{id}/edit', 'OrderListController@edit')->name('orders.edit');
+        Route::post('/orders/update', 'OrderListController@update')->name('orders.update');
+        Route::get('/orders/{id}', 'OrderListController@order')->name('orders.order');
+        Route::get('/orders', 'OrderListController@orderList')->name('orders.orders');
+        /**
+         * User Routes
+         */
+        Route::get('/users', 'UserListController@userList')->name('users.users');
     });
 });
