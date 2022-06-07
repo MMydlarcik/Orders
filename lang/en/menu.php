31c8297f52b9535c79cfd051830e5615b9c3f529
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-07 18:35:34 +0200

    Create menu.php

---
+++ lang/en/menu.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'home' => 'Home',
+    'orders' => 'Orders',
+    'users' => 'Users',
+    'login' => 'Log In',
+    'logout' => 'Log Out',
+    'signup' => 'Sign Up',
+
+];
