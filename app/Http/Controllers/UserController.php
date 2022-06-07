ba49224a466c6d9e3e9a0ea18c2559950929a606
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-07 18:33:41 +0200

    Create UserController.php

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function userList() 
+    {
+        $users = User::all();
+        return view('users.users')->with([
+            'users'=> $users
+        ]);
+    }
+
+    public function user($id)
+    {
+        $user = User::find($id);
+        return view('users.show')->with('user', $user);
+    }
+
+    public function destroy(Request $request)
+    {
+        $id = $request->input('id');
+        User::destroy($id);
+        return redirect(route('users.users'))->with('success', 'Success');
+    }
+
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('users.edit')->with('user', $user);
+    }
+
+
+    public function update(Request $request)
+    {   
+        $id = $request->input('id');
+        $username = $request->input('username');
+        $email = $request->input('email');
+
+        $user = User::find($id);
+        $user->update([
+            'username' => $username,
+            'email' => $email
+        ]);
+        return redirect(route('users.users'));
+    }
+
+    public function updatePassword(Request $request)
+    {   
+        $id = $request->input('id');
+        $password = $request->input('password');
+        $user = User::find($id);
+        $user->update([
+            'password' => $password
+        ]);
+        return redirect(route('users.users'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $username = $request->input('username');
+        $email = $request->input('email');
+        $password = $request->input('password');
+
+
+        User::create([
+            'username' => $username,
+            'email' => $email,
+            'password' => $password,
+        ]);
+        return redirect(route('users.users'))->with('success', 'Success');
+    }
+}
