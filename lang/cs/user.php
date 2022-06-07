bb80cdc73f240d730cdf4d1d0b5a1e8fed32139f
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-07 18:35:24 +0200

    Create user.php

---
+++ lang/cs/user.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'title' => 'Seznam uživatelů',
+    'id' => 'ID',
+    'username' => 'Uživatelské jméno',
+    'email' => 'E-mail',
+    'action' => 'Akce',
+    'view' => 'Detail',
+    'add' => 'Přidat nového uživatele',    
+];
