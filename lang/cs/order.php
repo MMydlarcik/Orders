88aad2120a13ffe3ba0f38bb067f5b73eb03da04
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-07 18:34:42 +0200

    Create order.php

---
+++ lang/cs/order.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'id' => 'ID',
+    'code' => 'Kód',
+    'authorId' => 'ID autora',
+    'action' => 'Akce',
+    'view' => 'Detail',
+    'add' => 'Přidat novou objednávku',
+    'title' => 'Seznam objednávek',
+    'create_success' => 'Objednávka přidána!',
+    'destroy_success' => 'Objednávka vymazána!',
+
+];
