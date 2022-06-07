9170032f4f497aca3164bb78d99d8d6a5a73095c
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-07 18:35:10 +0200

    Create passwords.php

---
+++ lang/cs/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Vaše heslo musí být změněno!',
+    'sent' => 'Odeslali jsem vám odkaz ke změně hesla!',
+    'throttled' => 'Prosíme, vyčkejte před dalším pokusem.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "Uživatel s tímto heslem nenalezen.",
+
+];
