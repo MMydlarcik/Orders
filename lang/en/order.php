f1fa9d4dd008d9409292bbb73890b0e79ab6bfcd
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-07 18:35:39 +0200

    Create order.php

---
+++ lang/en/order.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'id' => 'ID',
+    'code' => 'Code',
+    'authorId' => 'Author ID',
+    'action' => 'Action',
+    'view' => 'View',
+    'add' => 'Add New',
+    'title' => 'Orders List',
+    'create_success' => 'Order added!',
+
+    
+];
