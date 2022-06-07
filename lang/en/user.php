5fbec5795a5db9a2616854b1ad24e1d3d7d74842
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-07 18:35:43 +0200

    Create user.php

---
+++ lang/en/user.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'title' => 'Users List',
+    'id' => 'ID',
+    'username' => 'Username',
+    'email' => 'E-mail',
+    'action' => 'Action',
+    'view' => 'View',
+    'add' => 'Add New',    
+];
