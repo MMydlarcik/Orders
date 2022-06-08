2a08cf6339984e4cea791e53edbd0ea0fa0e57de
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-08 19:49:48 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() 
     {
-        return view('home.index');
+        $lastOrders = Order::latest()->take(5)->get();
+        return view('home.index')->with('lastOrders', $lastOrders);
     }
 }
