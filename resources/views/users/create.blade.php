cd148b1bcb28d3921446baffdeba607efd3a4042
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-11 20:51:36 +0200

    Update create.blade.php

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,20 +1,33 @@
 @extends('layouts.app-master')
 @section('content')
-<div class="card">
-  <div class="card-header">New User</div>
-  <div class="card-body">
+    <div class="card">
+        <div class="card-header">New User</div>
+        <div class="card-body">
 
-    <form action="{{ route ('users.store') }}" method="post">
-      {!! csrf_field() !!}
-      <label>Username</label></br>
-      <input type="text" name="username" id="username" class="form-control"></br>
-      <label>E-mail</label></br>
-      <input type="email" name="email" id="email" class="form-control"></br>
-      <label>Password`</label></br>
-      <input type="password" name="password" id="password" class="form-control"></br>
-      <input type="submit" value="Save" class="btn btn-success"></br>
-    </form>
+            <form action="{{ route('users.store') }}" method="post">
+                {!! csrf_field() !!}
+                <label>Username</label></br>
+                <input type="text" name="username" id="username" class="form-control"></br>
+                <label>E-mail</label></br>
+                <input type="email" name="email" id="email" class="form-control"></br>
+                @if (Auth::user()->role == 'admin')
+                <label>Role</label></br>
+                <select name="role" id="role" class="form-control"></br>
+                    <option value="admin">Admin</option>
+                    <option value="user">User</option>
+                </select>
+                @endif
+                @if (Auth::user()->role == 'user')
+                <label>Role</label></br>
+                <select name="role" id="role" class="form-control"></br>
+                    <option value="user">User</option>
+                </select>
+                @endif
+                <label>Password</label></br>
+                <input type="password" name="password" id="password" class="form-control"></br>
+                <input type="submit" value="Save" class="btn btn-success"></br>
+            </form>
 
-  </div>
-</div>
+        </div>
+    </div>
 @endsection
