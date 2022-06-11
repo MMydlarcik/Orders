9ff9d61f7fbaf324b8592327553865d17480f27e
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-11 20:51:41 +0200

    Update edit.blade.php

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,28 +1,43 @@
 @extends('layouts.app-master')
 @section('content')
-<div class="card">
-  <div class="card-header">Edit user</div>
-  <div class="card-body">
+    <div class="card">
+        <div class="card-header">Edit user</div>
+        <div class="card-body">
 
-    <form action="{{ route('users.update') }}" method="post">
-      {!! csrf_field() !!}
-      <input type="hidden" name="id" id="id" value="{{ $user->getId() }}" id="id" />
-      <label>Username</label></br>
-      <input type="text" name="username" id="username" value="{{ $user->getUsername() }}" class="form-control"></br>
-      <label>Email</label></br>
-      <input type="text" name="email" id="email" value="{{ $user->getEmail() }}" class="form-control"></br>
-      <input type="submit" value="Update" class="btn btn-success"></br>
-    </form>
+            <form action="{{ route('users.update') }}" method="post">
+                {!! csrf_field() !!}
+                <input type="hidden" name="id" id="id" value="{{ $user->getId() }}" id="id" />
+                <label>Username</label></br>
+                <input type="text" name="username" id="username" value="{{ $user->getUsername() }}"
+                    class="form-control"></br>
+                <label>Email</label></br>
+                <input type="text" name="email" id="email" value="{{ $user->getEmail() }}" class="form-control"></br>
+                @if (Auth::user()->role == 'admin')
+                    <label>Role</label></br>
+                    <select name="role" id="role" class="form-control"></br>
+                        <option value="admin">Admin</option>
+                        <option value="user">User</option>
+                    </select>
+                @endif
+                @if (Auth::user()->role == 'user')
+                    <label>Role</label></br>
+                    <select name="role" id="role" class="form-control"></br>
+                        <option value="user">User</option>
+                    </select>
+                @endif
+                <input type="submit" value="Update" class="btn btn-success"></br>
+            </form>
 
-    <br><hr><br>
-    
-    <form action="{{ route('users.updatePassword') }}" method="post">
-      {!! csrf_field() !!}
-      <input type="hidden" name="id" id="id" value="{{ $user->getId() }}" id="id" />
-      <label>Password</label></br>
-      <input type="password" name="password" id="password" value="" class="form-control"></br>
-      <input type="submit" value="Update" class="btn btn-success"></br>
-    </form>
-  </div>
-</div>
+            <br>
+            <hr><br>
+
+            <form action="{{ route('users.updatePassword') }}" method="post">
+                {!! csrf_field() !!}
+                <input type="hidden" name="id" id="id" value="{{ $user->getId() }}" id="id" />
+                <label>Password</label></br>
+                <input type="password" name="password" id="password" value="" class="form-control"></br>
+                <input type="submit" value="Update" class="btn btn-success"></br>
+            </form>
+        </div>
+    </div>
 @endsection
