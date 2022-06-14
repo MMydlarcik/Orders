8c5a287edcc3f96a6eeb04e47ac24c65ea6b12e3
MMydlarcik <milan.mydlarcik@seznam.cz>
2022-06-14 15:15:06 +0200

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,15 +11,33 @@ class Order extends Model
     protected $primaryKey = 'id';
     protected $fillable = ['code', 'author_id'];
 
-    public function getId (){
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getCode (){
+    public function getCode()
+    {
         return $this->code;
     }
 
-    public function getAuthorId (){
+    public function getAuthorId()
+    {
         return $this->author_id;
+    }
+
+    public function author()
+    {
+        return $this->hasOne(User::class, 'id', 'author_id');
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class, 'order_id', 'id');
+    }
+
+    public function historyItems()
+    {
+        return $this->hasMany(OrderHistory::class, 'order_id', 'id');
     }
 }
